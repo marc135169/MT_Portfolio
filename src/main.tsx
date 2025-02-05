5114badbdc1c3529795322036d844dc2cf0a5beb
marc135169 <marc.tetart2511@gmail.com>
2025-02-05 18:18:09 +0100

    Base Main

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,40 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// React
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+// Style
+import './index.css';
+
+// Pages
+import App from './App.tsx';
+
+// Children
+import Home from './pages/Home.tsx';
+
+const router = createBrowserRouter([
+    {
+        element: <App />,
+        children: [
+            {
+                path: '/',
+                element: <Home />,
+            },
+            /*{
+                path: '/recipe/:id',
+                element: < />,
+            },
+            {
+                path: '*',
+                element: < />,
+            },*/
+        ],
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
