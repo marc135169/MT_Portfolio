ec90daf7292cd8d45955cec84d5a9fe7fc663006
marc135169 <marc.tetart2511@gmail.com>
2025-02-19 12:44:26 +0100

    Home Page

--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -1,7 +1,13 @@
-export default function Project(){
+import React from "react";
+import Slider from "../components/Slider";
+
+const Projects: React.FC = () => {
     return (
-        <main>
-            <h1 className="text-3xl pt-20 text-primary">MES PROJETS</h1>
-        </main>
-    )
-}
+        <section className="py-20 px-5">
+            <h2 className="text-3xl font-bold mb-6 text-primary">Mes Projets</h2>
+            <Slider />
+        </section>
+    );
+};
+
+export default Projects;
